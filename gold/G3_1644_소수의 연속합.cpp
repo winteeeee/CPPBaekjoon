5eed9dab4ffad2ff4010fdc31ed3d82938729cca
winteeeee <b12e34e56@naver.com>
2022-09-18 18:06:38 +0900

    G3 1644 소수의 연속합 해결

--- gold/G3_1644_소수의 연속합.cpp
+++ gold/G3_1644_소수의 연속합.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+bool prime[40000001];
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -8,36 +10,46 @@ int main() {
 
     int n;
     cin >> n;
+    if(n == 2)
+        cout << 1;
 
-    bool prime[n + 1];
-    for(int i = 0; i <= n; i++)
-        prime[i] = true;
+    else {
+        for (int i = 0; i <= n; i++)
+            prime[i] = true;
 
-    for(int i = 2; i <= n / 2; i++) {
-        for(int j = 2; i * j <= n; j++) {
-            prime[i * j] = false;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; i * j <= n; j++) {
+                prime[i * j] = false;
+            }
         }
+
+        vector<int> primes;
+        for (int i = 1; i <= n; i++) {
+            if (prime[i])
+                primes.push_back(i);
+        }
+
+        int count = 0;
+        for (int i = 2; i < primes.size(); i++) {
+            if (primes[i] == n)
+                count++;
+
+            else {
+                int prefixSum = primes[i] + primes[i - 1];
+                for (int j = i - 1; j >= 1;) {
+                    if (prefixSum < n) {
+                        prefixSum += primes[--j];
+                    } else if (prefixSum > n) {
+                        break;
+                    } else {
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        cout << count;
     }
-
-    int count = 0;
-    int start = 1;
-    int end = 2;
-    int sum = prime[start] + prime[end];
-    while(end <= n || start < end) {
-        if(prime[start] + prime[end] < n) {
-            sum += prime[++end];
-        }
-
-        else if(prime[start] + prime[end] > n) {
-            sum -= prime[start++];
-        }
-
-        else {
-            sum -= prime[start++];
-            count++;
-        }
-    }
-
-    cout << count;
     return 0;
 }
