a579a587799724b1bd514f228cf9b8de8a67c519
winteeeee <b12e34e56@naver.com>
2022-09-19 14:57:08 +0900

    G4 1647 도시 분할 계획

--- gold/G3_1644_소수의 연속합.cpp
+++ gold/G3_1644_소수의 연속합.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -52,4 +53,4 @@ int main() {
         cout << count;
     }
     return 0;
-}
+}*/
--- gold/G4 1647 도시 분할 계획.cpp
+++ gold/G4 1647 도시 분할 계획.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+#include <queue>
 using namespace std;
 
+const int SIZE = 1000001;
+vector<pair<int, int>> road[SIZE];
+bool visited[SIZE] = {false};
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> h;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -10,12 +16,38 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<pair<int, int>> road[m + 1];
     for(int i = 0; i < m; i++) {
         int a, b, c;
+        cin >> a >> b >> c;
         road[a].push_back({b, c});
         road[b].push_back({a, c});
     }
+    h.push({0, 1});
 
+    int max = INT_MIN;
+    int sum = 0;
+    for(int j = 0; j < n;) {
+        pair<int, int> cur = h.top(); h.pop();
+        int min = cur.first;
+        int minIdx = cur.second;
+        if(visited[minIdx])
+            continue;
+
+        j++;
+        sum += min;
+        visited[minIdx] = true;
+        if(max < min)
+            max = min;
+
+        for(int i = 0; i < road[minIdx].size(); i++) {
+            int nextIdx = road[minIdx][i].first;
+            int nextWeight = road[minIdx][i].second;
+            if(!visited[nextIdx]) {
+                h.push({nextWeight, nextIdx});
+            }
+        }
+    }
+
+    cout << sum - max;
     return 0;
 }
