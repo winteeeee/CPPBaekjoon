6dbab19a643c5553eedcb30a30932cbb0f9632be
wintee <b12e34e56@naver.com>
2022-09-21 15:02:33 +0900

    G3 2473 세 용액 해결

--- gold/G3 2473 세 용액.cpp
+++ gold/G3 2473 세 용액.cpp
@@ -1,9 +1,10 @@
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
 using namespace std;
 
+typedef long long ll;
 int n;
 vector<int> property;
 void input();
@@ -30,43 +31,57 @@ void input() {
 void solve() {
     cout.tie(NULL);
     sort(property.begin(), property.end());
-    int almostZero = INT_MAX;
-    int almostZeroIdx = 0;
-    for(int i = 0; i < property.size(); i++) {
-        if(abs(almostZero) > abs(property[i])) {
-            almostZero = property[i];
-            almostZeroIdx = i;
-        }
-    }
 
-    property.erase(property.begin() + almostZeroIdx);
     int result[3];
-    int prev = INT_MAX;
-    int start = 0;
-    int end = property.size() - 1;
-    while(start < end) {
-        int sum = property[start] + property[end];
-        if(prev > abs(sum + almostZero)) {
-            prev = abs(sum);
-            result[0] = property[start];
-            result[1] = property[end];
-        }
+    ll tempSum = 3000000001;
+    for(int i = 0; i < property.size(); i++) {
+        ll prev = 3000000001;
+        int tempStartIdx;
+        int tempEndIdx;
+        int almostZero = property[i];
+        int almostZeroIdx = i;
+        int start = (0 != i) ? 0 : 1;
+        int end = (property.size() - 1 != i) ?  property.size() - 1 :  property.size() - 2;
 
-        if(almostZero + sum != 0) {
-            int temp1 = property[start] + property[end - 1];
-            int temp2 = property[start + 1] + property[end];
+        while (start < end) {
+            ll sum = property[start] + property[end];
+            if (prev > abs(sum + almostZero)) {
+                prev = abs(sum + almostZero);
+                tempStartIdx = start;
+                tempEndIdx = end;
+            }
 
-            if(abs(temp1 + almostZero) < abs(temp2 + almostZero))
-                end--;
+            if (almostZero + sum != 0) {
+                ll temp1 = property[start] + property[end - 1];
+                ll temp2 = property[start + 1] + property[end];
+
+                if (abs(temp1 + almostZero) < abs(temp2 + almostZero)) {
+                    if(end - 1 != almostZeroIdx)
+                        end--;
+                    else
+                        end -= 2;
+                }
+
+                else {
+                    if(start + 1 != almostZeroIdx)
+                        start++;
+                    else
+                        start += 2;
+                }
+            }
+
             else
-                start++;
+                break;
         }
 
-        else
-            break;
+        if(prev < tempSum) {
+            tempSum = prev;
+            result[0] = property[tempStartIdx];
+            result[1] = property[tempEndIdx];
+            result[2] = almostZero;
+        }
     }
 
-    result[2] = almostZero;
     sort(result, result + 3);
     cout << result[0] << " " << result[1] << " " << result[2];
-}
+}*/
