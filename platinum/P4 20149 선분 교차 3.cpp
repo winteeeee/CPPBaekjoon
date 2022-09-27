9586bb2bf4ff56c4bf53d205bd534cca0eb6c6ff
winteeeee <b12e34e56@naver.com>
2022-09-27 18:13:01 +0900

    P4 20149 선분 교차 3 해결 중

--- platinum/P4 20149 선분 교차 3.cpp
+++ platinum/P4 20149 선분 교차 3.cpp
@@ -9,12 +9,12 @@ private:
     pair<double, double> point2;
 
 public:
-    Segment(int x1, int y1, int x2, int y2);
+    Segment(double x1, double y1, double x2, double y2);
     pair<double, double> getPoint1();
     pair<double, double> getPoint2();
 };
 
-Segment::Segment(int x1, int y1, int x2, int y2) {
+Segment::Segment(double x1, double y1, double x2, double y2) {
     point1 = {x1, y1};
     point2 = {x2, y2};
 }
@@ -36,7 +36,7 @@ int main() {
 
     vector<Segment> segments;
     for(int i = 0; i < 2; i++) {
-        int x1, y1, x2, y2;
+        double x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
         segments.push_back(Segment(x1, y1, x2, y2));
     }
@@ -44,47 +44,100 @@ int main() {
     if(isCross(segments[0].getPoint1(), segments[0].getPoint2(), segments[1].getPoint1(), segments[1].getPoint2())) {
         cout << "1" << "\n";
 
-        double temp1 = segments[0].getPoint2().second - segments[0].getPoint1().second;
-        double temp2 = segments[0].getPoint2().first - segments[0].getPoint1().first;
-        double temp3 = segments[1].getPoint2().second - segments[1].getPoint1().second;
-        double temp4 = segments[1].getPoint2().first - segments[1].getPoint1().first;
+        double x1 = segments[0].getPoint1().first;
+        double x2 = segments[0].getPoint2().first;
+        double x3 = segments[1].getPoint1().first;
+        double x4 = segments[1].getPoint2().first;
+        double y1 = segments[0].getPoint1().second;
+        double y2 = segments[0].getPoint2().second;
+        double y3 = segments[1].getPoint1().second;
+        double y4 = segments[1].getPoint2().second;
+        double temp1 = y2 - y1;
+        double temp2 = x2 - x1;
+        double temp3 = y4 - y3;
+        double temp4 = x4 - x3;
         double seg1Angle = temp1 / temp2;
         double seg2Angle = temp3 / temp4;
-        double x = abs((seg1Angle * segments[0].getPoint1().first * -1 + segments[0].getPoint1().second) - (seg2Angle * segments[1].getPoint1().first * - 1 + segments[1].getPoint1().second)) / abs(seg1Angle - seg2Angle);
-        double y = seg1Angle * x - seg1Angle * segments[0].getPoint1().first + segments[0].getPoint1().second;
+        double x = abs((seg1Angle * x1 * -1 + y1) - (seg2Angle * x3 * - 1 + y3)) / abs(seg1Angle - seg2Angle);
+        double y = seg1Angle * x - seg1Angle * x1 + y1;
+        cout.precision(9);
+
 
         if(!isnan(x) && !isnan(y) && seg1Angle != seg2Angle)
             cout << x << " " << y;
 
-        else if(isnan(x) && isnan(y) && seg1Angle == seg2Angle) {
-            if(segments[0].getPoint1().first == segments[1].getPoint1().first && segments[0].getPoint1().second == segments[1].getPoint1().second) {
-                if(segments[0].getPoint2().first < segments[0].getPoint1().first && segments[0].getPoint1().first < segments[1].getPoint2().first ||
-                   segments[1].getPoint2().first < segments[0].getPoint1().first && segments[0].getPoint1().first < segments[0].getPoint2().first)
-                    if(segments[0].getPoint2().first != segments[1].getPoint2().first)
-                        cout << segments[0].getPoint1().first << " " << segments[0].getPoint1().second;
+        else if(isnan(x) && isnan(y) && seg1Angle == seg2Angle && !isinf(seg1Angle) && !isinf(seg2Angle)) {
+            if(x1 == x3 && y1 == y3) {
+                if(x2 < x1 && x1 < x4 ||
+                   x4 < x1 && x1 < x2)
+                    if(x2 != x4)
+                        cout << x1 << " " << y1;
             }
 
-            if(segments[0].getPoint1().first == segments[1].getPoint2().first && segments[0].getPoint1().second == segments[1].getPoint2().second) {
-                if(segments[0].getPoint2().first < segments[0].getPoint1().first && segments[0].getPoint1().first < segments[1].getPoint1().first ||
-                   segments[1].getPoint1().first < segments[0].getPoint1().first && segments[0].getPoint1().first < segments[0].getPoint2().first) {
-                    if(segments[0].getPoint2().first != segments[1].getPoint1().first)
-                        cout << segments[0].getPoint1().first << " " << segments[0].getPoint1().second;
+            if(x1 == x4 && y1 == y4) {
+                if(x2 < x1 && x1 < x3 ||
+                   x3 < x1 && x1 < x2) {
+                    if(x2 != x3)
+                        cout << x1 << " " << y1;
                 }
             }
 
-            if(segments[0].getPoint2().first == segments[1].getPoint1().first && segments[0].getPoint2().second == segments[1].getPoint1().second) {
-                if(segments[0].getPoint1().first < segments[0].getPoint2().first && segments[0].getPoint2().first < segments[1].getPoint2().first ||
-                   segments[1].getPoint2().first < segments[0].getPoint2().first && segments[0].getPoint2().first < segments[0].getPoint1().first) {
-                    if(segments[0].getPoint1().first != segments[1].getPoint2().first)
-                        cout << segments[0].getPoint2().first << " " << segments[0].getPoint2().second;
+            if(x2 == x3 && y2 == y3) {
+                if(x1 < x2 && x2 < x4 ||
+                   x4 < x2 && x2 < x1) {
+                    if(x1 != x4)
+                        cout << x2 << " " << y2;
                 }
             }
 
-            if(segments[0].getPoint2().first == segments[1].getPoint2().first && segments[0].getPoint2().second == segments[1].getPoint2().second) {
-                if(segments[0].getPoint1().first < segments[0].getPoint2().first && segments[0].getPoint2().first < segments[1].getPoint1().first ||
-                   segments[1].getPoint1().first < segments[0].getPoint2().first && segments[0].getPoint2().first < segments[0].getPoint1().first) {
-                    if(segments[0].getPoint1().first != segments[1].getPoint1().first)
-                        cout << segments[0].getPoint2().first << " " << segments[0].getPoint2().second;
+            if(x2 == x4 && y2 == y4) {
+                if(x1 < x2 && x2 < x3 ||
+                   x3 < x2 && x2 < x1) {
+                    if(x1 != x3)
+                        cout << x2 << " " << y2;
+                }
+            }
+        }
+
+        else if(isinf(seg1Angle) || isinf(seg2Angle)) {
+            if(isinf(seg1Angle) && !isinf(seg2Angle)) {
+                cout << x1 << " " << seg2Angle * x1 - seg2Angle * x3 + y3;
+            }
+
+            else if(!isinf(seg1Angle) && isinf(seg2Angle)) {
+                cout << x3 << " " << seg1Angle * x3 - seg1Angle * x1 + y1;
+            }
+
+            else {
+                if(x1 == x3 && y1 == y3) {
+                    if(y2 < y1 && y1 < y4 ||
+                       y4 < y1 && y1 < y2)
+                        if(y2 != y4)
+                            cout << x1 << " " << y1;
+                }
+
+                if(x1 == x4 && y1 == y4) {
+                    if(y2 < y1 && y1 < y3 ||
+                       y3 < y1 && y1 < y2) {
+                        if(y2 != y3)
+                            cout << x1 << " " << y1;
+                    }
+                }
+
+                if(x2 == x3 && y2 == y3) {
+                    if(y1 < y2 && y2 < y4 ||
+                       y4 < y2 && y2 < y1) {
+                        if(y1 != y4)
+                            cout << x2 << " " << y2;
+                    }
+                }
+
+                if(x2 == x4 && y2 == y4) {
+                    if(y1 < y2 && y2 < y3 ||
+                       y3 < y2 && y2 < y1) {
+                        if(y1 != y3)
+                            cout << x2 << " " << y2;
+                    }
                 }
             }
         }
@@ -110,8 +163,8 @@ long long ccw(pair<double, double> a, pair<double, double> b, pair<double, doubl
 }
 
 bool isCross(pair<double, double> a, pair<double, double> b, pair<double, double> c, pair<double, double> d) {
-    int seg1 = ccw(a, b, c) * ccw(a, b, d);
-    int seg2 = ccw(c, d, a) * ccw(c, d, b);
+    long long seg1 = ccw(a, b, c) * ccw(a, b, d);
+    long long seg2 = ccw(c, d, a) * ccw(c, d, b);
 
     if(seg1 <= 0 && seg2 <= 0) {
         if ((a.first > c.first && a.first > d.first && b.first > c.first && b.first > d.first) ||
