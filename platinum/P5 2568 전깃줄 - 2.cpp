64be57b5d61c25408e4d1afd38d795d940143607
wintee <b12e34e56@naver.com>
2022-10-05 04:08:18 +0900

    P5 2568 전깃줄 - 2 해결 중

--- platinum/P5 2568 전깃줄 - 2.cpp
+++ platinum/P5 2568 전깃줄 - 2.cpp
@@ -1,10 +1,10 @@
 /*
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<pair<int, int>> wire;
-int lis[100001];
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -19,8 +19,53 @@ int main() {
         wire.push_back({a, b});
     }
 
-    lis[0] = wire[0].second;
+    sort(wire.begin(), wire.end());
+    vector<int> result;
+    int prevIdx = 0;
+    for(int i = 0; i < n; i++) {
+        pair<int, int> cur = wire[i];
 
+        if(wire[prevIdx].second > cur.second) {
+            result.push_back(cur.first);
+            continue;
+        }
+
+        if(i != n - 1) {
+            if(wire[i + 1].second < cur.second) {
+                result.push_back(cur.first);
+                if(prevIdx == 0)
+                    prevIdx = i + 1;
+                continue;
+            }
+        }
+    }
+
+    int size = result.size();
+    cout << size << "\n";
+    for(int i = 0; i < size; i++)
+        cout << result[i] << "\n";
 
     return 0;
-}*/
+}
+
+*/
+/*
+ * 아래 반례를 어떻게 처리할지 고민해볼것
+9
+1 50000
+2 4
+3 11
+4 12
+5 6
+6 3
+7 2
+8 9
+9 10
+
+5
+1
+3
+4
+6
+7
+ */
