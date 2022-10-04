29b06f08f9979a6a71203566bafc775b4f456c18
wintee <b12e34e56@naver.com>
2022-10-05 03:22:06 +0900

    B5 2420 사파리월드 해결

---
+++ platinum/P5 2568 전깃줄 - 2.cpp
@@ -0,0 +1,26 @@
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int>> wire;
+int lis[100001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        wire.push_back({a, b});
+    }
+
+    lis[0] = wire[0].second;
+
+
+    return 0;
+}*/
