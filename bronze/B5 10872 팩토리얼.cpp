60d5d19d01274b46df5543d8ae39dc2d9b39945b
winteeeee <b12e34e56@naver.com>
2022-10-09 02:01:44 +0900

    B5 10872 팩토리얼 해결

---
+++ bronze/B5 10872 팩토리얼.cpp
@@ -0,0 +1,19 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int result = 1;
+    for(int i = n; i >= 1; i--)
+        result *= i;
+
+    cout << result;
+    return 0;
+}*/
