b30975d3d5d95ecad3f58812c15aaa224f506df1
winteeeee <b12e34e56@naver.com>
2022-10-11 12:03:58 +0900

    G2 9257 1의 개수 세기 해결 중

---
+++ gold/G2 9527 1의 개수 세기.cpp
@@ -0,0 +1,34 @@
+/*#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+
+
+    return 0;
+}*/
+
+/*
+0000
+0001
+0010
+0011
+0100
+0101
+0110
+0111
+1000
+1001
+1010
+1011
+1100
+1101
+1110
+1111
+ */
