70e6d57a37cc8869ef870cbcbbe727394d6a5ce9
winteeeee <b12e34e56@naver.com>
2022-10-14 02:58:14 +0900

    B5 5597 과제 안 내신 분..? 해결

---
+++ bronze/B5 5597 과제 안 내신 분.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    bool arr[31] = {false};
+    for(int i = 0; i < 28; i++) {
+        int temp;
+        cin >> temp;
+        arr[temp] = true;
+    }
+
+    for(int i = 1; i <= 30; i++) {
+        if(!arr[i])
+            cout << i << "\n";
+    }
+
+    return 0;
+}*/
