052e736d8b9dae5bf550dd0e8ae49f9fb90abf82
winteeeee <b12e34e56@naver.com>
2022-10-17 00:26:23 +0900

    G1 14289 본대 산책 3 해결

--- gold/G1 14289 본대 산책 3.cpp
+++ gold/G1 14289 본대 산책 3.cpp
@@ -4,38 +4,37 @@
 using namespace std;
 typedef long long ll;
 
-int N, m, d;
-const int MOD = 1e9 + 7;
-vector<vector<ll>> pow(vector<vector<ll>> building, int n);
+int n, m, d;
+const ll MOD = 1e9 + 7;
+vector<vector<ll>> pow(vector<vector<ll>> building, int num);
 vector<vector<ll>> mul(vector<vector<ll>> g1, vector<vector<ll>> g2);
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> N >> m;
-    vector<vector<ll>> building(N, vector<ll>(N, 0));
+    cin >> n >> m;
+    vector<vector<ll>> building(n, vector<ll>(n, 0));
     for(int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         a--; b--;
-        building[a][b] = 1;
-        building[b][a] = 1;
+        building[a][b] = building[b][a] = 1;
     }
 
     cin >> d;
     vector<vector<ll>> resultBuilding = pow(building, d);
 
     ll result = resultBuilding[0][0];
-    cout << result % MOD;
+    cout << result;
     return 0;
 }
 
-vector<vector<ll>> pow(vector<vector<ll>> building, int n) {
-    if(n == 1)
+vector<vector<ll>> pow(vector<vector<ll>> building, int num) {
+    if(num == 1)
         return building;
     else {
-        vector<vector<ll>> temp = pow(building, n / 2);
-        if(n & 1)
+        vector<vector<ll>> temp = pow(building, num / 2);
+        if(num & 1)
             return mul(mul(temp, temp), building);
         else
             return mul(temp, temp);
@@ -43,16 +42,16 @@ vector<vector<ll>> pow(vector<vector<ll>> building, int n) {
 }
 
 vector<vector<ll>> mul(vector<vector<ll>>g1, vector<vector<ll>> g2) {
-    vector<vector<ll>> result(N, vector<ll>(N, 0));
+    vector<vector<ll>> result(n, vector<ll>(n, 0));
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            for(int k = 0; k < N; k++) {
-                result[i][j] += (g1[i][k] % MOD * g2[k][j] % MOD % MOD);
+    for(int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                result[i][j] += (g1[i][k] * g2[k][j]);
+                result[i][j] %= MOD;
             }
         }
     }
 
     return result;
-}
-*/
+}*/
