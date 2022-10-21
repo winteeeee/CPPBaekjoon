0fd4d03671cb41524bd0cfe5d1ebf323b30954d0
winteeeee <b12e34e56@naver.com>
2022-10-22 02:37:54 +0900

    B5 9086 문자열 해결

---
+++ bronze/B5 9086 문자열.cpp
@@ -0,0 +1,21 @@
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++) {
+        string temp;
+        cin >> temp;
+        cout << temp[0] << temp[temp.size() - 1] << endl;
+    }
+
+    return 0;
+}*/
