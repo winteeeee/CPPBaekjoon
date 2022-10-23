70f3a9e25d31d3800983f70d1281acb783738e4b
wintee <b12e34e56@naver.com>
2022-10-24 03:00:17 +0900

    B5 立方体 (Cube) 해결

--- bronze/B5 24082 立方体 (Cube).cpp
+++ bronze/B5 24082 立方体 (Cube).cpp
@@ -1,20 +1,15 @@
 /*
 #include <iostream>
-typedef long long ll;
 using namespace std;
 
-ll cal(ll a, ll b);
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    ll a, b;
-    cin >> a >> b;
-    cout << cal(a, b);
+    int x;
+    cin >> x;
+    cout << x * x * x;
 
     return 0;
-}
-ll cal(ll a, ll b) {
-    return (a + b) * (a - b);
 }*/
