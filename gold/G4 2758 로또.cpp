cd8c2e4f4db6b80004209f58695c8e6dfe918518
winteeeee <b12e34e56@naver.com>
2022-11-29 14:50:45 +0900

    G4 2758 해결 중 좀 더 시간을 줄여야할듯함

--- gold/G4 2758 로또.cpp
+++ gold/G4 2758 로또.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 long long count = 0;
 int t, n, m;
-void counting(int cur, int selectCount) {
+void counting(double cur, int selectCount) {
     if(selectCount >= n) {
         count++;
         return;
     }
 
     for(int i = cur * 2; i <= m; i++) {
-        counting(i, selectCount + 1);
+        if(i * pow(2, n - (selectCount + 1) < m))
+            counting(i, selectCount + 1);
     }
 }
 
@@ -23,7 +25,7 @@ int main() {
 
     for(int i = 0; i < t; i++) {
         cin >> n >> m;
-        counting(1, 1);
+        counting(0.5, 0);
         cout << count << '\n';
         count = 0;
     }
