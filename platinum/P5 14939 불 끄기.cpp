fce6d36f0107e396374ed9c3e10af66319ecfc3e
wintee <b12e34e56@naver.com>
2023-01-09 22:59:18 +0900

    P5 14939 다시 시작

--- platinum/P5 14939 불 끄기.cpp
+++ platinum/P5 14939 불 끄기.cpp
@@ -1,77 +1,69 @@
-/*#include <iostream>
-#include <queue>
+#include <iostream>
 using namespace std;
 
-bool bulb[10][10];
-int coordX[5] = {0, 0, 0, 1, -1};
-int coordY[5] = {0, 1, -1, 0, 0};
-class Switches {
-public:
-    int r;
-    int c;
-    int offTargetBulbs;
-    void setOffTargetBulbsNCoord() {
-        int count = 0;
-        for(int i = 0; i < 5; i++) {
-            if((0 <= r + coordY[i] && r + coordY[i] < 10) && (0 <= c + coordX[i] && c + coordX[i] < 10)) {
-                if(bulb[r + coordY[i]][c + coordX[i]])
-                    count++;
-                else
-                    count--;
-            }
-        }
-
-        offTargetBulbs = count;
-    }
-};
-
-class comp {
-public:
-    bool operator() (const Switches &a, const Switches &b) {
-        return a.offTargetBulbs < b.offTargetBulbs;
-    }
-};
+bool table[10][10];
+int xRange[5] {-1, 1, 0, 0, 0};
+int yRange[5] {0, 0, -1, 1, 0};
+void input();
+void solve();
+void convertSwitch(int i, int j);
+void print();
+long long answer;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    input();
+    solve();
+}
+
+void input() {
+    char temp;
+
     for(int i = 0; i < 10; i++) {
-        string temp;
-        cin >> temp;
         for(int j = 0; j < 10; j++) {
-            if(temp[j] == '#')
-                bulb[i][j] = 0;
-            else
-                bulb[i][j] = 1;
-        }
-    }
+            cin >> temp;
 
-    int count = 0;
-    while(1) {
-        priority_queue<Switches, vector<Switches>, comp> h;
+            if(temp == '#') {
+                table[i][j] = 0;
+            }
 
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++) {
-                Switches cur = {i, j};
-                cur.setOffTargetBulbsNCoord();
-                h.push(cur);
+            else {
+                table[i][j] = 1;
             }
         }
-        
-        int curTargetNum = h.top().offTargetBulbs;
-        if(curTargetNum <= -1)
-            break;
-
-        for(int i = 0; i < 5; i++)
-            bulb[h.top().r + coordY[i]][h.top().c + coordX[i]] ^= 1;
-        count++;
     }
+}
 
-    cout << count;
-    return 0;
-}*/
+void solve() {
+
+}
+
+void convertSwitch(int i, int j) {
+    for(int k = 0; k < 5; k++) {
+        if((0 <= i + yRange[k] && i + yRange[k] < 10) && (0 <= j + xRange[k] && j + xRange[k] < 10)) {
+            table[i + yRange[k]][j + xRange[k]] = !table[i + yRange[k]][j + xRange[k]];
+        }
+    }
+}
+
+void print() {
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            if(table[i][j]) {
+                cout << 'O';
+            }
+
+            else {
+                cout << '#';
+            }
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
 
 /*
 ##########
