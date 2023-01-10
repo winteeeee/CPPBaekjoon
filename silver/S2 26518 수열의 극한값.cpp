d7c7dfac8884d24a7845685a19bd4e29e1887255
wintee <b12e34e56@naver.com>
2023-01-10 18:15:50 +0900

    S2 26518 수열의 극한값 해결

---
+++ silver/S2 26518 수열의 극한값.cpp
@@ -0,0 +1,27 @@
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+
+ll a1, a2, b, c;
+void input();
+void solve();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+    solve();
+}
+
+void input() {
+    cin >> b >> c >> a1 >> a2;
+}
+
+void solve() {
+    cout.precision(9);
+    cout << fixed;
+    cout << (b + sqrt(b*b + 4 * c)) / 2;
+}*/
