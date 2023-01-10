2e0e70d6d62ed0b2ae433e7847ace692fb02c979
wintee <b12e34e56@naver.com>
2023-01-10 16:47:52 +0900

    S5 26517 연속인가? ? 해결

---
+++ silver/S5 26517 연속인가.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+using namespace std;
+
+long long a, b, c, d, k;
+void input();
+void solve();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+    solve();
+}
+
+void input() {
+    cin >> k >> a >> b >> c >> d;
+}
+
+void solve() {
+    long long leftVal = a * k + b;
+    long long rightVal = c * k + d;
+
+    if(leftVal == rightVal) {
+        cout << "Yes " << leftVal;
+    }
+
+    else {
+        cout << "No";
+    }
+}*/
