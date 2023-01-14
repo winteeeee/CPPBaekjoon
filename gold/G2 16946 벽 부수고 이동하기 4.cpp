966493cc0dd521d90b46c8b06465a0109c4fd0f2
winteeeee <b12e34e56@naver.com>
2023-01-14 14:00:33 +0900

    G2 16946 벽 부수고 이동하기 4 해결 중

--- gold/G2 16946 벽 부수고 이동하기 4.cpp
+++ gold/G2 16946 벽 부수고 이동하기 4.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
 int n, m;
-bool isWall[1000][1000];
-int result[1000][1000];
+int isWall[1000][1000];
+int zeroCount[1000][1000];
 int rowMoveSet[4] = {1, -1, 0, 0};
 int columnMoveSet[4] = {0, 0, 1, -1};
 
@@ -25,43 +26,81 @@ void input() {
     }
 }
 
-int countMovable(int r, int c) {
+void dfs(int r, int c, vector<pair<int, int>> &path, int &pathIndex, bool visited[][1000], int &count) {
+    visited[r][c] = 1;
+
+    for(int i = 0; i < 4; i++) {
+        if((0 <= r + rowMoveSet[i] && r + rowMoveSet[i] < n) &&
+           (0 <= c + columnMoveSet[i] && c + columnMoveSet[i] < m)) {
+            if(!visited[r + rowMoveSet[i]][c + columnMoveSet[i]] &&
+            !isWall[r + rowMoveSet[i]][c + columnMoveSet[i]]) {
+                path[pathIndex++] = {r + rowMoveSet[i], c + columnMoveSet[i]};
+                count++;
+                dfs(r + rowMoveSet[i], c + columnMoveSet[i], path, pathIndex, visited, count);
+            }
+        }
+    }
+}
+
+void countMovable(int r, int c) {
+    bool isPlus[1000][1000] = {0};
     bool visited[1000][1000] = {0};
-    int count = 1;
     queue<pair<int, int>> q;
     q.push({r, c});
+    visited[r][c] = 1;
 
     while(!q.empty()) {
         pair<int, int> cur = q.front(); q.pop();
 
         for(int i = 0; i < 4; i++) {
             if((0 <= cur.first + rowMoveSet[i] && cur.first + rowMoveSet[i] < n) &&
-            (0 <= cur.second + columnMoveSet[i] && cur.second + columnMoveSet[i] < m)) {
-                if(!visited[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]] &&
-                !isWall[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]]) {
-                    count++;
-                    visited[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]] = 1;
-                    q.push({cur.first + rowMoveSet[i], cur.second + columnMoveSet[i]});
+               (0 <= cur.second + columnMoveSet[i] && cur.second + columnMoveSet[i] < m)) {
+                if(isWall[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]]) {
+                    if(!isPlus[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]]) {
+                        isPlus[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]] = 1;
+                        isWall[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]] += zeroCount[r][c];
+                    }
+                } else {
+                    if(!visited[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]]) {
+                        visited[cur.first + rowMoveSet[i]][cur.second + columnMoveSet[i]] = 1;
+                        q.push({cur.first + rowMoveSet[i], cur.second + columnMoveSet[i]});
+                    }
                 }
             }
         }
     }
-
-    return count;
 }
 
-void solve() {
+void countZero() {
+    bool visited[1000][1000] = {0};
+    vector<pair<int, int>> path(1000000);
+    int pathIndex = 0;
+    int count = 1;
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(isWall[i][j]) {
-                result[i][j] = countMovable(i, j) % 10;
+            if(!isWall[i][j] && !visited[i][j]) {
+                path[pathIndex++] = {i, j};
+                dfs(i, j, path, pathIndex, visited, count);
+
+                for(int k = 0; k < pathIndex; k++) {
+                    zeroCount[path[k].first][path[k].second] = count;
+                }
+
+                count = 1;
+                pathIndex = 0;
+                countMovable(i, j);
             }
         }
     }
+}
+
+void solve() {
+    countZero();
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cout << result[i][j];
+            cout << isWall[i][j] % 10;
         }
         cout << '\n';
     }
