5c4834e9fd1ce8ef48aedbeb64de55144a19d297
winteeeee <b12e34e56@naver.com>
2023-01-17 17:49:51 +0900

    보드게임 컵 E번 해결 실패

--- boardGameCup_1/E번 - 벚꽃 내리는 시대에 결투를.cpp
+++ boardGameCup_1/E번 - 벚꽃 내리는 시대에 결투를.cpp
@@ -1,12 +1,13 @@
-/*
 #include <iostream>
 #include <vector>
 #include <string>
+#include <climits>
 using namespace std;
 
-int n, a, l, sumOfLifeDamages;
-long long beTakenADamages;
-vector<pair<int, int>> v;
+int n, a, l;
+vector<pair<int, int>> v(5001);
+vector<bool> path[5001];
+int dp[5001][5001];
 
 void input() {
     cin >> n >> a >> l;
@@ -15,89 +16,73 @@ void input() {
         int x, y;
         cin >> x >> y;
 
-        v.push_back({x, y});
-        sumOfLifeDamages += y;
+        v[i] = {x, y};
 
-        if(y == -1) {
-            beTakenADamages += x;
-        }
-    }
-}
-
-void dfs(int start, int curA, int curL, string path, int sumOfLifeDamage, long long beTakenADamage) {
-    for(int i = start; i < n; i++) {
-        pair<int, int> attack = v[i];
-
-        if(sumOfLifeDamage < curL) {
-            cout << "YES" << '\n';
-
-            for(int j = i; j < n; j++) {
-                pair<int, int> attack2 = v[j];
-                if (attack2.second == -1) {
-                    path += "A";
-                } else {
-                    path += "L";
-                }
-            }
-
-            cout << path;
-            exit(0);
-        }
-
-        if(attack.second == -1) {
-            curA = curA - attack.first < 0 ? 0 : curA - attack.first;
-            beTakenADamage -= attack.first;
-            path += "A";
-
-            if((curA - beTakenADamage <= 0 && curL < sumOfLifeDamage) || (curL <= 0)) {
-                return;
-            }
-
-            dfs(start + 1, curA, curL, path, sumOfLifeDamage, beTakenADamage);
-        } else if(attack.first == -1) {
-            curL -= attack.second;
-            sumOfLifeDamage -= attack.second;
-            path += "L";
-
-            if((curA - beTakenADamage <= 0 && curL < sumOfLifeDamage) || (curL <= 0)) {
-                return;
-            }
-
-            dfs(start + 1, curA, curL, path, sumOfLifeDamage, beTakenADamage);
-        } else {
-            if(curA < attack.first) {
-                curL -= attack.second;
-                sumOfLifeDamage -= attack.second;
-                path += "L";
-
-                if((curA - beTakenADamage <= 0 && curL < sumOfLifeDamage) || (curL <= 0)) {
-                    return;
-                }
-
-                dfs(start + 1, curA, curL, path, sumOfLifeDamage, beTakenADamage);
-            } else {
-                string aPath = path + "A";
-                string lPath = path + "L";
-                sumOfLifeDamage -= attack.second;
-
-                if((curA - beTakenADamage <= 0 && curL < sumOfLifeDamage) || (curL <= 0)) {
-                    return;
-                }
-
-                dfs(start + 1, curA - attack.first, curL, aPath, sumOfLifeDamage, beTakenADamage);
-                dfs(start + 1, curA, curL - attack.second, lPath, sumOfLifeDamage, beTakenADamage);
-            }
+        for(int j = 0; j < 5001; j++) {
+            dp[i][j] = INT_MIN;
         }
     }
 
-    cout << "YES" << '\n' << path;
-    exit(0);
+    for(int j = 0; j < 5001; j++) {
+        dp[n][j] = INT_MIN;
+    }
 }
 
 void solve() {
-    string result = "NO";
-    dfs(0, a, l, "", sumOfLifeDamages, beTakenADamages);
-    cout << result;
+    const bool A = 0;
+    const bool L = 1;
+    dp[0][l] = a;
+
+    for(int i = 0; i < n; i++) {
+        pair<int, int> attack = v[i];
+
+        for(int life = 1; life <= l; life++) {
+            if(dp[i][life] != INT_MIN) {
+                if (attack.first == -1 && life - attack.second > 0) {
+                    dp[i + 1][life - attack.second] = max(dp[i][life], dp[i + 1][life - attack.second]);
+                    path[life - attack.second] = path[life];
+                    path[life - attack.second].push_back(L);
+                } else if (attack.second == -1) {
+                    dp[i + 1][life] = max(dp[i + 1][life], max(dp[i][life] - attack.first, 0));
+                    path[life].push_back(A);
+                } else {
+                    if (life - attack.second > 0) {
+                        dp[i + 1][life - attack.second] = max(dp[i][life], dp[i + 1][life - attack.second]);
+                        path[life - attack.second] = path[life];
+                        path[life - attack.second].push_back(L);
+                    }
+
+                    if (dp[i][life] >= attack.first) {
+                        dp[i + 1][life] = max(dp[i][life] - attack.first, dp[i + 1][life]);
+
+                        if(attack.second != 0) {
+                            path[life].push_back(A);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for(int life = 1; life <= l; life++) {
+        if(dp[n][life] >= 0) {
+            cout << "YES\n";
+
+            string resultPath;
+            for(int i = 0; i < path[life].size(); i++) {
+                if(path[life][i]) {
+                    resultPath += "L";
+                } else {
+                    resultPath += "A";
+                }
+            }
+
+            cout << resultPath;
+            exit(0);
+        }
+    }
+
+    cout << "NO";
 }
 
 int main() {
@@ -107,4 +92,4 @@ int main() {
 
     input();
     solve();
-}*/
+}
