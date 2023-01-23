8bcc9459f821d1d747adcb2dcf9e7716ea87d961
wintee <b12e34e56@naver.com>
2023-01-23 13:45:15 +0900

    G1 17143 낚시왕 해결

--- gold/G1 17143 낚시왕.cpp
+++ gold/G1 17143 낚시왕.cpp
@@ -1,9 +1,8 @@
+/*
 #include <iostream>
 #include <vector>
 #include <queue>
 using namespace std;
-
-static int count = 0;
 
 class shark {
 public:
@@ -20,14 +19,18 @@ public:
     }
 
     bool operator<(const shark s) const {
-        if(this->pos.second == s.pos.second) {
-            return this->size < s.size;
-        } else {
-            return this->pos.second > s.pos.second;
+        if(this->pos.first == s.pos.first) {
+            return this->size > s.size;
         }
+
+        return this->pos.first > s.pos.first;
     }
 };
 
+const int UP = 1;
+const int DOWN = 2;
+const int RIGHT = 3;
+const int LEFT = 4;
 int R, C, M;
 priority_queue<shark> h[101];
 
@@ -42,14 +45,14 @@ void input() {
 }
 
 void changeDirection(shark &s) {
-    if(s.direction == 1) {
-        s.direction = 2;
-    } else if(s.direction == 2) {
-        s.direction = 1;
-    } else if(s.direction == 3) {
-        s.direction = 4;
+    if(s.direction == UP) {
+        s.direction = DOWN;
+    } else if(s.direction == DOWN) {
+        s.direction = UP;
+    } else if(s.direction == RIGHT) {
+        s.direction = LEFT;
     } else {
-        s.direction = 3;
+        s.direction = RIGHT;
     }
 }
 
@@ -60,28 +63,74 @@ void moving() {
         while(!h[i].empty()) {
             shark cur = h[i].top(); h[i].pop();
 
-            if(cur.direction == 1) {
+            int leftBlock = cur.speed;
+            int movableBlock;
+            pair<int, int> curPos = cur.pos;
+            while(leftBlock > 0) {
+                if (cur.direction == UP) {
+                    movableBlock = curPos.first - 1;
 
-            } else if(cur.direction == 2) {
+                    if(movableBlock > leftBlock) {
+                        curPos.first -= leftBlock;
+                    } else {
+                        curPos.first -= movableBlock;
+                        changeDirection(cur);
+                    }
+                } else if (cur.direction == DOWN) {
+                    movableBlock = R - curPos.first;
 
-            } else if(cur.direction == 3) {
+                    if(movableBlock > leftBlock) {
+                        curPos.first += leftBlock;
+                    } else {
+                        curPos.first += movableBlock;
+                        changeDirection(cur);
+                    }
+                } else if (cur.direction == RIGHT) {
+                    movableBlock = C - curPos.second;
 
-            } else {
+                    if(movableBlock > leftBlock) {
+                        curPos.second += leftBlock;
+                    } else {
+                        curPos.second += movableBlock;
+                        changeDirection(cur);
+                    }
+                } else {
+                    movableBlock = curPos.second - 1;
 
+                    if(movableBlock > leftBlock) {
+                        curPos.second -= leftBlock;
+                    } else {
+                        curPos.second -= movableBlock;
+                        changeDirection(cur);
+                    }
+                }
+                leftBlock -= movableBlock;
             }
 
+            cur.pos = curPos;
             temp.push_back(cur);
         }
     }
 
+
     for(int i = 0; i < temp.size(); i++) {
-        if(h[temp[i].pos.second].size()) {
-            while(h[temp[i].pos.second].top().pos.first == temp[i].pos.first) {
-                h[temp[i].pos.second].pop();
-            }
+        h[temp[i].pos.second].push(temp[i]);
+    }
+
+    for(int i = 1; i <= C; i++) {
+        vector<shark> temp2;
+
+        while(!h[i].empty()) {
+            temp2.push_back(h[i].top());
+            h[i].pop();
         }
 
-        h[temp[i].pos.second].push(temp[i]);
+        for(int j = 1; j <= temp2.size(); j++) {
+            if(j == temp2.size() ||
+            !(temp2[j - 1].pos.first == temp2[j].pos.first && temp2[j - 1].pos.second == temp2[j].pos.second)) {
+                h[i].push(temp2[j - 1]);
+            }
+        }
     }
 }
 
@@ -89,8 +138,10 @@ void solve() {
     int sum = 0;
 
     for(int i = 1; i <= C; i++) {
-        sum += h[i].top().size;
-        if(h[i].size()) {
+        if(!h[i].empty()) {
+            shark gotIt = h[i].top();
+
+            sum += gotIt.size;
             h[i].pop();
         }
         moving();
@@ -106,4 +157,4 @@ int main() {
 
     input();
     solve();
-}
+}*/
