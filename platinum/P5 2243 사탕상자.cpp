ec3a7de9c728b139bf4466563fc0eeee120e8601
wintee <b12e34e56@naver.com>
2023-02-27 21:31:49 +0900

    P5 2243 사탕상자 해결

--- platinum/P5 2243 사탕상자.cpp
+++ platinum/P5 2243 사탕상자.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,9 +12,9 @@ public:
     int C;
 };
 const int ROOT = 1;
-const int MAX = 1000000;
+const int MAX = 1000001;
 vector<order> orders;
-vector<int> candyBox((MAX + 1) * 4);
+vector<int> candyBox(MAX * 4);
 
 void input() {
     cin >> n;
@@ -37,8 +38,12 @@ int update(int start, int end, int index, int target, int count) {  //A가 2일 
         return candyBox[index];
     }
 
-    if (start == end && start == target) {
-        return candyBox[index] += count;
+    if (start == end) {
+        if (start == target) {
+            return candyBox[index] += count;
+        } else {
+            return candyBox[index];
+        }
     }
 
     int mid = (start + end) / 2;
@@ -47,8 +52,19 @@ int update(int start, int end, int index, int target, int count) {  //A가 2일 
     return candyBox[index] = left + right;
 }
 
-int findCandy(int index, int target) {   //A가 1일 때
+int findCandy(int start, int end, int index, int target) {   //A가 1일 때
+    if (start == end) {
+        return start;
+    }
 
+    int left = candyBox[index * 2];
+    int mid = (start + end) / 2;
+
+    if (left < target) {    //오른쪽 서브트리를 선택한다면 오른쪽 서브트리 내의 target은 left만큼 빠진다.
+        return findCandy(mid + 1, end, index * 2 + 1, target - left);
+    } else {
+        return findCandy(start, mid, index * 2, target);
+    }
 }
 
 void solve() {
@@ -56,8 +72,9 @@ void solve() {
         order cur = orders[i];
 
         if (cur.A == 1) {
-            cout << findCandy(ROOT, cur.B) << '\n';
-            update(ROOT, MAX, ROOT, cur.B, -1);
+            int findedCandy = findCandy(ROOT, MAX, ROOT, cur.B);
+            cout << findedCandy << '\n';
+            update(ROOT, MAX, ROOT, findedCandy, -1);
         } else {
             update(ROOT, MAX, ROOT, cur.B, cur.C);
         }
@@ -72,4 +89,4 @@ int main() {
     input();
     solve();
     return 0;
-}
+}*/
