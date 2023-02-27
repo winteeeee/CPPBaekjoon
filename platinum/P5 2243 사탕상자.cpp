e57a245ee34b30de5d10c2d91806622d96cba569
wintee <b12e34e56@naver.com>
2023-02-27 15:53:58 +0900

    P5 2243 사탕상자 해결 중

--- platinum/P5 2243 사탕상자.cpp
+++ platinum/P5 2243 사탕상자.cpp
@@ -4,14 +4,16 @@
 using namespace std;
 
 int n;
-class candy {
+class order {
 public:
     int A;
     int B;
     int C;
 };
-vector<candy> orders;
-vector<int> candyBox;
+const int ROOT = 1;
+const int MAX = 1000000;
+vector<order> orders;
+vector<int> candyBox((MAX + 1) * 4);
 
 void input() {
     cin >> n;
@@ -25,30 +27,39 @@ void input() {
             cin >> C;
             orders.push_back({A, B, C});
         } else {
-            orders.push_back({A, B - 1});
+            orders.push_back({A, B});
         }
     }
 }
 
+int update(int start, int end, int index, int target, int count) {  //A가 2일 때
+    if (target < start || target > end) {
+        return candyBox[index];
+    }
+
+    if (start == end && start == target) {
+        return candyBox[index] += count;
+    }
+
+    int mid = (start + end) / 2;
+    int left = update(start, mid, index * 2, target, count);
+    int right = update(mid + 1, end, index * 2 + 1, target, count);
+    return candyBox[index] = left + right;
+}
+
+int findCandy(int index, int target) {   //A가 1일 때
+
+}
+
 void solve() {
     for (int i = 0; i < orders.size(); i++) {
-        candy cur = orders[i];
+        order cur = orders[i];
 
-        if (cur.A == 1) {   //꺼내기
-            cout << candyBox[cur.B] << '\n';
-            copy(candyBox.begin() + cur.B + 1, candyBox.end(), candyBox.begin() + cur.B);
-            candyBox.pop_back();
-        } else {    //넣기
-            if (cur.C > 0) {    //넣기
-                vector<int> temp(cur.C, cur.B);
-                candyBox.resize(candyBox.size() + cur.C);
-                copy(temp.begin(), temp.end(), candyBox.end() - cur.C);
-                sort(candyBox.begin(), candyBox.end());
-            } else {    //꺼내기
-                int targetIndex = lower_bound(candyBox.begin(), candyBox.end(), cur.B) - candyBox.begin();
-                copy(candyBox.begin() + targetIndex + (cur.C * -1), candyBox.end(), candyBox.begin() + targetIndex);
-                candyBox.resize(candyBox.size() + cur.C);
-            }
+        if (cur.A == 1) {
+            cout << findCandy(ROOT, cur.B) << '\n';
+            update(ROOT, MAX, ROOT, cur.B, -1);
+        } else {
+            update(ROOT, MAX, ROOT, cur.B, cur.C);
         }
     }
 }
