ee049e56d7086a441c4cccda96deb8e2d48714f6
wintee <b12e34e56@naver.com>
2023-03-05 02:53:00 +0900

    G1 1019 책 페이지 해결 시작

---
+++ gold/G1 1019 책 페이지.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int result[10];
+
+void input() {
+    cin >> n;
+}
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+    solve();
+    return 0;
+}
