cfe597a059bfdd673aad603fa60e67fa4e889a0f
winteeeee <b12e34e56@naver.com>
2023-03-10 12:30:09 +0900

    G1 1019 책 페이지 규칙 찾는 중

--- gold/G1 1019 책 페이지.cpp
+++ gold/G1 1019 책 페이지.cpp
@@ -9,7 +9,21 @@ void input() {
 }
 
 void solve() {
+    for(int i = 1; i <= n; i++) {
+        string cur = to_string(i);
 
+        for(int j = 0; j < cur.size(); j++) {
+            result[cur[j] - '0']++;
+        }
+
+        if (i % 1000 == 0) {
+            cout << i << " : ";
+            cout << result[0] << " " << result[1] << " " << result[2] << " " << result[3] << " " << result[4] << " " << result[5] << " " << result[6] << " " << result[7] << " " << result[8] << " " << result[9];
+            cout << '\n';
+        }
+    }
+    cout << "최종 결과 : ";
+    cout << result[0] << " " << result[1] << " " << result[2] << " " << result[3] << " " << result[4] << " " << result[5] << " " << result[6] << " " << result[7] << " " << result[8] << " " << result[9];
 }
 
 int main() {
