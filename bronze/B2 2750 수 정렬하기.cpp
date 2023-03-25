aa6ee3c0f8202bcafe398e39a46f0c82707d2855
wintee <b12e34e56@naver.com>
2023-03-25 11:59:01 +0900

    B2 2750 수 정렬하기 해결

---
+++ bronze/B2 2750 수 정렬하기.cpp
@@ -0,0 +1,36 @@
+/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<int> arr;
+
+void input() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+}
+
+void solve() {
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    input();
+    solve();
+
+    return 0;
+}*/
