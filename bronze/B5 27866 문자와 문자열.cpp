3894d9f1b464eb37f2e7722ba7a2bf15a8e0571e
winteeeee <b12e34e56@naver.com>
2023-06-23 20:19:31 +0900

    B5 27866 문자와 문자열 해결

---
+++ bronze/B5 27866 문자와 문자열.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int i;
+string s;
+
+void input() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> s >> i;
+}
+
+void solve() {
+    cout << s[i - 1];
+}
+
+int main() {
+    input();
+    solve();
+
+    return 0;
+}
