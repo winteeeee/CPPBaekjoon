bbf1f83c78a78adadb3c07e9883c9c2932158636
winteeeee <b12e34e56@naver.com>
2023-06-28 00:45:40 +0900

    G3 14725 개미굴 해결

---
+++ gold/G3 14725 개미굴.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+class Node {
+public:
+    string id;
+    vector<Node*> children;
+    Node(string i) {
+        id = i;
+    }
+};
+
+int n;
+Node root = {"root"};
+stack<Node*> s;
+
+void input() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int count;
+        cin >> count;
+
+        Node* curNode = &root;
+        for (int j = 0; j < count; j++) {
+            string temp;
+            cin >> temp;
+
+            bool isFind = false;
+            for (int k = 0; k < curNode->children.size(); k++) {
+                if (curNode->children[k]->id == temp) {
+                    isFind = true;
+                    curNode = curNode->children[k];
+                    break;
+                }
+            }
+
+            if (!isFind) {
+                Node* newNode = new Node(temp);
+                curNode->children.push_back(newNode);
+                curNode = newNode;
+            }
+        }
+    }
+}
+
+bool comparator(const Node* a, const Node* b) {
+    return a->id < b->id;
+}
+
+void treePrint(Node* n) {
+    sort(n->children.begin(), n->children.end(), comparator);
+
+    for (int i = 0; i < n->children.size(); i++) {
+        for (int j = 0; !s.empty() && j < s.size(); j++) {
+            cout << "--";
+        }
+        cout << n->children[i]->id << '\n';
+        s.push(n->children[i]);
+        treePrint(n->children[i]);
+        s.pop();
+    }
+}
+
+void solve() {
+    treePrint(&root);
+}
+
+int main() {
+    input();
+    solve();
+
+    return 0;
+}
--- gold/G3 20303 할로윈의 양아치.cpp
+++ gold/G3 20303 할로윈의 양아치.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -85,3 +86,4 @@ int main() {
 
     return 0;
 }
+*/
