b0e21edda99c2aec717a139cc5656da4192ad29b
winteeeee <b12e34e56@naver.com>
2023-07-04 02:16:09 +0900

    P2 9206 나무 말고 꽃 해결

--- platinum/P2 9206 나무 말고 꽃.cpp
+++ platinum/P2 9206 나무 말고 꽃.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -14,35 +15,15 @@ double long v;
 int n;
 vector<Vase> vases;
 
-double long risingFactorial(double long a, int num) {
-    if (num == 0) {
-        return 1;
-    }
-
-    double long result = 1;
-    for (int i = 0; i < num; i++) {
-        result *= (a + i);
-    }
-
-    return result;
-}
-
 double long exponentialIntegralE(double long x) {
-    /*
-     * Exponential Integral을 구하기 위하여
-     * Incomplete gamma function을 구해야함.
-     * 이를 구하기 위해 Confluent Hypergeometric function 사용
-     */
-
     double long sigma = -1;
     double long nextVal = 0;
-    for (int i = 0; nextVal != sigma; i++) {
+    for (int i = 0; sigma != nextVal; i++) {
         sigma = nextVal;
-        nextVal += (risingFactorial(0.75, i) * pow(-x, i)) / (risingFactorial(1.75, i) * tgamma(i + 1));
-        //tgamma(n + 1) == n!
+        nextVal += pow(x, i) / tgamma(1.75 + i);
     }
 
-    return pow(x, -0.75) * (tgamma(0.75) - (1.33333 * pow(x, 0.75) * sigma));
+    return pow(x, -0.75) * (tgamma(0.75) - pow(x, 0.75) * sigma * tgamma(0.75) * pow(M_E, -x));
 }
 
 double long a(const Vase& vase) {
@@ -50,12 +31,6 @@ double long a(const Vase& vase) {
 }
 
 double long b(const Vase& vase) {
-    double long check1 = 2 * vase.b;
-    double long check2 = -2 * vase.a * pow(vase.h, 1.5);
-    double long check3 = exponentialIntegralE(pow(vase.h, 2));
-    double long check4 = vase.b * pow(vase.h, 2);
-    double long check5 = 2 * vase.a * tgamma(0.75);
-
     return 2 * vase.b * (-2 * vase.a * pow(vase.h, 1.5) * exponentialIntegralE(pow(vase.h, 2)) + 2 * vase.a * tgamma(0.75) + vase.b * pow(vase.h, 2));
 }
 
@@ -76,11 +51,9 @@ int main() {
     }
 
     double long minDiff = abs(v - findVolume(vases[0]));
-    cout << findVolume(vases[0]) << '\n';
     int result = 0;
     for (int i = 1; i < n; i++) {
         double long volume = findVolume(vases[i]);
-        cout << volume << '\n';
 
         if (abs(v - volume) < minDiff) {
             minDiff = abs(v - volume);
@@ -91,3 +64,4 @@ int main() {
     cout << result;
     return 0;
 }
+*/
