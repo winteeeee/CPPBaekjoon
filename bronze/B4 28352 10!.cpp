1aaf3a5ee995b23b1ccbc23fe2cd4bea2bff8da0
winteeeee <b12e34e56@naver.com>
2023-07-22 01:17:32 +0900

    B4 28352 10! 해결

---
+++ bronze/B4 28352 10!.cpp
@@ -0,0 +1,27 @@
+/*
+#include <iostream>
+using namespace std;
+
+long long calcFac(long long n) {
+    long long result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin >> n;
+    long long factorial = calcFac(n);
+    cout << factorial / 604800;
+
+    return 0;
+}
+*/
--- bronze/B5 27866 문자와 문자열.cpp
+++ bronze/B5 27866 문자와 문자열.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -21,4 +22,4 @@ int main() {
     solve();
 
     return 0;
-}
+}*/
